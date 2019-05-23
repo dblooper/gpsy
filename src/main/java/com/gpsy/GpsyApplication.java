431e946baeb041c6b4cac4370e7063a4462d6820
dkoplenski <dkoplenski@gmail.com>
2019-05-23 19:04:52 +0200

    First commit

---
+++ src/main/java/com/gpsy/GpsyApplication.java
@@ -0,0 +1,13 @@
+package com.gpsy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GpsyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GpsyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gpsy/GpsyApplicationTests.java
@@ -0,0 +1,16 @@
+package com.gpsy;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GpsyApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
